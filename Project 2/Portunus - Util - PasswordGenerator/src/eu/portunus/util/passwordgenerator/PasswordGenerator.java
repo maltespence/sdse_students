af51c9cd71564389f6db48bde0323756c521bb46
maltespence <maltespence@gmail.com>
2020-10-26 21:50:13 +0100

    Cleaned up PassWordGenerator: Removed switch statement

--- Project 2/Portunus - Util - PasswordGenerator/src/eu/portunus/util/passwordgenerator/PasswordGenerator.java
+++ Project 2/Portunus - Util - PasswordGenerator/src/eu/portunus/util/passwordgenerator/PasswordGenerator.java
@@ -9,42 +9,14 @@ public class PasswordGenerator implements IPasswordGenerator {
 	
 	HashMap<CharacterSet,String> characterMap = new HashMap<CharacterSet,String>();
 	
-	public void generateCharacterMap(HashSet<CharacterSet> characterSets) {
+	public void generateCharacterMap() {
 		// Generate hashmap containing only allowed characters
-		for(CharacterSet charset : characterSets) {
-			switch(charset) {
-			case DIGITS:
-				if(!characterMap.containsKey(charset)) {
-					characterMap.put(CharacterSet.DIGITS,"0123456789");
-				}
-				break;
-			case LOWER_CASE:
-				if(!characterMap.containsKey(charset)) {
-				characterMap.put(CharacterSet.LOWER_CASE,"abcdefghijklmnopqrstuvwxyz");
-				}
-				break;
-			case UPPER_CASE:
-				if(!characterMap.containsKey(charset)) {
-				characterMap.put(CharacterSet.UPPER_CASE,"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-				}
-				break;
-			case MINUS:
-				if(!characterMap.containsKey(charset)) {
-				characterMap.put(CharacterSet.MINUS,"-");
-				}
-				break;
-			case UNDERSCORE:
-				if(!characterMap.containsKey(charset)) {
-				characterMap.put(CharacterSet.UNDERSCORE,"_");
-				}
-				break;
-			case SPACE:
-				if(!characterMap.containsKey(charset)) {
-				characterMap.put(CharacterSet.SPACE," ");
-				}
-				break;
-			}
-		}
+		characterMap.put(CharacterSet.DIGITS,"0123456789");
+		characterMap.put(CharacterSet.LOWER_CASE,"abcdefghijklmnopqrstuvwxyz");
+		characterMap.put(CharacterSet.UPPER_CASE,"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		characterMap.put(CharacterSet.MINUS,"-");
+		characterMap.put(CharacterSet.UNDERSCORE,"_");
+		characterMap.put(CharacterSet.SPACE," ");
 	}
 	
 	@Override
@@ -52,15 +24,16 @@ public class PasswordGenerator implements IPasswordGenerator {
 		String pw = "";
 		String characterString = "";
 		
-		// Check if charactersets have been set, and length is >= 0
+		// Check if characterSets have been set, and length is >= 0
 		if (characterSets == null || characterSets.isEmpty() || length<0) {
 			return "";
 		}
 
 		//Generate HashSet from characterSets to ensure no duplicates. Then generate hashmap
 		HashSet<CharacterSet> characterSetsCleaned = new HashSet<CharacterSet>(characterSets);
-		generateCharacterMap(characterSetsCleaned);
+		generateCharacterMap();
 		
+		//TODO: Implement an option that ensures that at least one of each characterset is used.
 		for(CharacterSet charset : characterSetsCleaned) {
 			characterString = characterString + characterMap.get(charset);
 		}
--- Project 2/Portunus - Util - PasswordGenerator/src/eu/portunus/util/passwordgenerator/test/PasswordGeneratorTest.java
+++ Project 2/Portunus - Util - PasswordGenerator/src/eu/portunus/util/passwordgenerator/test/PasswordGeneratorTest.java
@@ -12,22 +12,22 @@ public class PasswordGeneratorTest {
 	
 	// Check if password length matches the input required length
 	@Test
-	public void passwordLengthTest() {
+	public void passwordLengthCorrectTest() {
 		charsets.clear();
 		charsets.add(CharacterSet.LOWER_CASE);
 		String pw = pwgen.generatePassword(10,charsets);
 		assertEquals(10, pw.length());
 	}
 
-	// Check output if no charactersets provided. Output must be 0.
+	// Check output if no characterSets provided. Output must be 0.
 	@Test
-	public void passwordNoCharacterSetsTest() {
+	public void noCharacterSetsTest() {
 		charsets.clear();
 		String pw = pwgen.generatePassword(10,charsets);
 		assertEquals("", pw);
 	}
 	
-	// Check output if pw length is negative. Output must be empty.
+	// Check output if pw length is negative. Output must be empty string.
 	@Test
 	public void passwordLengthNegativeTest() {
 		charsets.clear();
@@ -37,10 +37,21 @@ public class PasswordGeneratorTest {
 		System.out.println("passwordLengthNegativeTest: " + pw);
 	}
 	
-	//TODO: Test if user can input the same collection multiple times. Must not be possible.
+	//Test if password length can be 0. Output must be empty string.
 	@Test
-	public void duplicateCharsetsTest() {
-		//TODO: Implement this test
+	public void passwordLengthZero() {
+		charsets.clear();
+		charsets.add(CharacterSet.DIGITS);
+		charsets.add(CharacterSet.LOWER_CASE);
+		charsets.add(CharacterSet.UPPER_CASE);
+		String pw = pwgen.generatePassword(0,charsets);
+		assertEquals("", pw);
+		
+	}
+	
+	//Test if user can input the same collection multiple times. Must not be possible.
+	@Test
+	public void duplicateCharacterSetsTest() {
 		charsets.clear();
 		charsets.add(CharacterSet.LOWER_CASE);
 		charsets.add(CharacterSet.LOWER_CASE);
@@ -48,6 +59,22 @@ public class PasswordGeneratorTest {
 		charsets.add(CharacterSet.UPPER_CASE);
 		String pw = pwgen.generatePassword(20,charsets);
 		System.out.println("duplicateCharsetsTest: " + pw);
-		
+		// I do not know how to automatically validate this test. As such, it is manual.
 	}
+	
+	@Test
+	public void allCharacterSets() {
+		charsets.clear();
+		charsets.add(CharacterSet.DIGITS);
+		charsets.add(CharacterSet.LOWER_CASE);
+		charsets.add(CharacterSet.UPPER_CASE);
+		charsets.add(CharacterSet.MINUS);
+		charsets.add(CharacterSet.UNDERSCORE);
+		charsets.add(CharacterSet.SPACE);
+		String pw = pwgen.generatePassword(20,charsets);
+		System.out.println("allCharacterSets: " + pw);
+		// I do not know how to automatically validate this test. As such, it is manual.
+		// I cannot guarantee, that one entry from each CharacterSet is part of the password.
+	}
+
 }
